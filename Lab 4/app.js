eb7b1ae7cdb5bce72eb8338f54fc3ae5b020dca7
Sachindu Gimhana <54847380+SachinduG@users.noreply.github.com>
2021-04-23 13:09:24 +0530

    lab sheet

--- Lab 4/app.js
+++ Lab 4/app.js
@@ -1,46 +1,60 @@
-//console.log('Hello World');
-
-// const os = require('os');
-//
-// console.log('Architecture ' + os.arch());
-// console.log('CPUs ' + os.cpus());
-// console.log('OS ' +os.platform());
-
-/*const fs = require('fs');
-const fileName = __dirname + '/test.txt';
-/!*const outFileName = __dirname + '/test-copy.txt';
-const readStream = fs.createReadStream(fileName);
-const writeStream = fs.createWriteStream(fileName);
-
-readStream.pipe(writeStream);
-
-readStream.on('data', data => {
-   console.log(data.toString());
-});*!/
-
-fs.readFile(fileName, (err, data) => {
-   if(err){
-      console.log(err);
-   }
-   console.log(data.toString());
-});*/
-
+const os = require('os');
+const fs = require('fs');
 const http = require('http');
 
+// Q 01
+// console.log('Hello World')
+
+// Q 02
+// console.log('Architecture ' + os.arch());
+// console.log('CPUs ' + os.cpus().length);
+// console.log('OS ' + os.platform());
+
+// Q 03
+// const fileName = __dirname + '/test.txt';
+// fs.readFile(fileName, (err, data) => {
+//     if(err) {
+//         console.log(err)
+//     }
+//     console.log(data.toString());
+// })
+
+// const data = fs.readFileSync(fileName);
+// console.log(data.toString());
+
+// Q 04
+// const fileName = __dirname + '/test.txt';
+// const outFileName = __dirname + '/test-copy.txt';
+// const readStream = fs.createReadStream(fileName);
+// const writeStream = fs.createWriteStream(outFileName);
+// readStream.on('data', data => {
+//     console.log(data.toString());
+// })
+// readStream.pipe(writeStream);
+
+// Q 05
+// http.createServer((req, res) => {
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<h1>Hello World</h1>');
+//     res.end();
+// }).listen(3000);
+
+/*
 http.createServer((req, res) => {
-   res.setHeader('Content-Type', 'text/html');
-   switch (req.method){
-      case 'GET':
-         res.write('<h1>Hello World</h1>');
-         res.end();
-         break;
-      case 'POST':
-         res.on('data', data => {
-            res.write('<h1>Hello ' +data+ '</h1>');
+    res.setHeader('Content-Type', 'text.html');
+    switch (req.method) {
+        case 'GET':
+            res.write('<h1>Hello World</h1>');
             res.end();
-         });
-         break;
-   }
+            break;
+        case 'POST':
+            req.on('data', data => {
+                res.write('<h1>Hello ' + data + '</h1>');
+                res.end();
+            });
+            break;
+    }
 }).listen(3000, (err) => {
-   console.log('Server is listening to port 3000')
+    console.log('Server is listening to port 3000')
 });
+*/
